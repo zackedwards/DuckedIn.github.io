547644bc89cb6c3b757bfc6be67a7cfff433233e
Zack <zacke@sent.com>
2021-04-30 12:38:49 -0400

    Updating the post method

--- newPost/newPost.js
+++ newPost/newPost.js
@@ -4,14 +4,21 @@ function postData(){
     let today = new Date().toLocaleDateString();
     console.log(today);
     
-    var data = document.getElementById('newpost').innerHTML;
+    var data = document.getElementById('newpost').value;
     console.log(data);
 
-    var title_data = document.getElementById('postTitle').innerHTML;
+    var title_data = document.getElementById('postTitle').value;
     console.log(title_data);
 
+    if(document.getElementById('privacyOption2').checked){
+        var option = document.getElementById('privacyOption2').value;
+    }
+    else{
+        var option = document.getElementById('privacyOption1').value;
+    }
+
     axios.post('https://sheetdb.io/api/v1/9kxufr2k05mi6?sheet=Post_Data',{
-        "data": {"Posted_By": readCookie('ISID'), "Title": title_data,"Body": data}
+        "data": {"Date _Posted": today, "Type_Of_Post": option, "Posted_By": readCookie('ISID'), "Title": title_data,"Body": data}
     }).then( response => {
         console.log(response.data);
     });
