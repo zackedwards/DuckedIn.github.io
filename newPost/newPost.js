324c46c9f09d1c7f645b3e1d44caac09e2c57946
Zack <zacke@sent.com>
2021-05-03 17:40:07 -0400

    Tring another await

--- newPost/newPost.js
+++ newPost/newPost.js
@@ -65,7 +65,7 @@ async function postData(){
     }
     console.log(option);
 
-    axios.post('https://sheetdb.io/api/v1/9kxufr2k05mi6?sheet=Post_Data',{
+    await axios.post('https://sheetdb.io/api/v1/9kxufr2k05mi6?sheet=Post_Data',{
         "data": [{"UPID": upid, "Date_Posted": today, "Type_Of_Post": option, "Posted_By": uname, "Title": title_data,"Body": data}]
     }).then( response => {
         console.log(response.data);
